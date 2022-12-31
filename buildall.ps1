5729e91a12de62a37dc5d4d55ae5ec2b4a7cc967
Harris <brakmic@gmail.com>
2022-12-31 15:29:47 +0100

    add windows compilation support

--- buildall.ps1
+++ buildall.ps1
@@ -4,3 +4,4 @@ meson setup builddir
 Copy-Item -Path .\index.html -Destination .\builddir\index.html -Force
 Copy-Item -Path .\vendor -Destination .\builddir\ -Recurse -Force
 meson compile -C builddir
+
