9c0d0cbb4a3514e03822490af01ebff2dfeb3e25
Harris <brakmic@gmail.com>
2023-01-01 22:51:31 +0100

    feat: use drogon config file

--- src/demo_web_server.cpp
+++ src/demo_web_server.cpp
@@ -6,16 +6,29 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char *argv[]) {
+/// @brief Will be executed after drogon::app().run().
+/// more info:
+/// https://github.com/drogonframework/drogon-docs/blob/master/ENG-12-AOP-Aspect-Oriented-Programming.md
+void displayNetworkInfo() {
+  auto listeners = drogon::app().getListeners();
+
+  for (const auto& l : listeners) {
+    std::cout << "web server listening on "
+              << fmt::format("http://{}:{}", l.toIp(), l.toIpPort())
+              << std::endl;
+  }
+}
+
+int main(int argc, char* argv[]) {
   argparse::ArgumentParser program("demo_web_server", "0.0.1");
 
   program.add_argument("-i", "--ip-address")
       .help("Server IP Address")
-      .default_value<std::string>({"127.0.0.1"});
+      .default_value(std::string{});
 
   program.add_argument("-p", "--port")
       .help("Port")
-      .default_value<unsigned int>(3000)
+      .default_value<unsigned int>(0)
       .scan<'d', unsigned int>();
 
   program.parse_args(argc, argv);
@@ -23,9 +36,14 @@ int main(int argc, char *argv[]) {
   auto ip{program.get<std::string>("ip-address")};
   auto port{program.get<unsigned int>("port")};
 
-  drogon::app().addListener(ip, port);
-  std::cout << "web server listening on "
-            << fmt::format("http://{}:{}", ip, port) << std::endl;
+  if (!ip.empty() && (port > 0)) {
+    drogon::app().addListener(ip, port);
+  }
+
+  drogon::app().loadConfigFile("config.json");
+
+  drogon::app().registerBeginningAdvice(displayNetworkInfo);
+
   drogon::app().run();
   return 0;
 }
