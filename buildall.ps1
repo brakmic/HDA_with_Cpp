cd93c4fc4b112e7faf660419df145391603fc615
Harris <brakmic@gmail.com>
2023-01-01 23:23:25 +0100

    fix: copy favicon

--- buildall.ps1
+++ buildall.ps1
@@ -2,6 +2,7 @@
 Remove-Item builddir -Force -Recurse
 meson setup builddir
 Copy-Item -Path .\index.html -Destination .\builddir\index.html -Force
+Copy-Item -Path .\cpp_favicon.ico -Destination .\builddir\cpp_favicon.ico -Force
 Copy-Item -Path .\styles.css -Destination .\builddir\styles.css -Force
 Copy-Item -Path .\vendor -Destination .\builddir\ -Recurse -Force
 Copy-Item -Path .\videos -Destination .\builddir\ -Recurse -Force
