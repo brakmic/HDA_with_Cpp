89f17a6fdba24c7b888c17fe2ff812a7f3c0b0f3
Harris <brakmic@gmail.com>
2023-01-01 15:27:32 +0100

    copy gifs to builddir

--- buildall.ps1
+++ buildall.ps1
@@ -4,4 +4,5 @@ meson setup builddir
 Copy-Item -Path .\index.html -Destination .\builddir\index.html -Force
 Copy-Item -Path .\styles.css -Destination .\builddir\styles.css -Force
 Copy-Item -Path .\vendor -Destination .\builddir\ -Recurse -Force
+Copy-Item -Path .\videos -Destination .\builddir\ -Recurse -Force
 meson compile -C builddir
