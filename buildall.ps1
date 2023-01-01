ec56d395b3008c1b7bf64af20c535453f3fbf597
Harris <brakmic@gmail.com>
2023-01-01 15:19:48 +0100

    add font-awesome, update index.html and build scripts

--- buildall.ps1
+++ buildall.ps1
@@ -2,6 +2,6 @@
 Remove-Item builddir -Force -Recurse
 meson setup builddir
 Copy-Item -Path .\index.html -Destination .\builddir\index.html -Force
+Copy-Item -Path .\styles.css -Destination .\builddir\styles.css -Force
 Copy-Item -Path .\vendor -Destination .\builddir\ -Recurse -Force
 meson compile -C builddir
-
