eef35f208cd8830f20530b31b592df4a9d72daaa
Harris <brakmic@gmail.com>
2023-01-02 21:53:37 +0100

    test: add tests

--- test/test_demo_web_server.cpp
+++ test/test_demo_web_server.cpp
@@ -2,13 +2,37 @@
 #include <criterion/logging.h>
 #include <string.h>
 
-void setup(void) { puts("Runs before the test"); }
+#include "../src/server/server_config.hpp"
 
-void teardown(void) { puts("Runs after the test"); }
+ReportHook(PRE_INIT)(struct criterion_test *test) {
+  printf("testing %s in category %s\n", test->name, test->category);
+}
 
-Test(sample, test) {
-  cr_expect(strlen("Test") == 4, "Expected \"Test\" to have a length of 4.");
-  // cr_expect(strlen("Hello") == 4, "This will always fail, why did I add
-  // this?");
-  cr_assert(strlen("") == 0);
+ReportHook(POST_TEST)(struct criterion_test_stats *stats) {
+  printf("Asserts: [%d passed, %d failed, %d total]\n", stats->passed_asserts,
+         stats->failed_asserts, stats->passed_asserts + stats->failed_asserts);
+}
+
+ReportHook(PRE_ALL)(struct criterion_test_set *tests) {
+  (void)tests;
+  setbuf(stdout, NULL);
+  setbuf(stderr, NULL);
+  puts("init");
+}
+
+ReportHook(POST_ALL)(struct criterion_global_stats *stats) {
+  (void)stats;
+  puts("end");
+}
+
+void setup(void) {}
+
+void teardown(void) {}
+
+Test(server_config, passing) {
+  ServerConfig config;
+  cr_expect(config.to_json().isNull() == false,
+            "ServerConfig should have read JSON");
+  cr_expect(config.get("database", "file").empty() == false,
+            "ServerConfig should return SQLite file from JSON");
 }
